944421c27d0dbb954e4ad0a8eb055a2275dfaf5e
Elder <eldergalvao07@gmail.com>
2024-09-27 14:06:17 -0300

    Adicionar arquivos de projeto.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace aulasMAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}

eff368814c17b8c30a0e8ef37082a37e0dce8e78
Elder <eldergalvao07@gmail.com>
2024-09-27 15:39:23 -0300

    addEventoBotoes

---
+++ addEventoBotoes.xaml.cs
@@ -0,0 +1,18 @@
+namespace aulasMAUI
+{
+    public partial class addEventoBotoes : ContentPage
+    {
+
+        public addEventoBotoes()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Clicked(object sender, EventArgs e)
+        {
+            DisplayAlert("Aviso", "Olá novamente, Mundo!", "Ok");
+
+        }
+    }
+
+}

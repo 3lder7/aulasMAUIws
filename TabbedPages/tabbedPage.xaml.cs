385e0cdb383fa008c22760ca5861c4c1a93aa364
Elder <eldergalvao07@gmail.com>
2024-10-01 20:21:25 -0300

    addTabbedPages

---
+++ TabbedPages/tabbedPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace aulasMAUI
+{
+    public partial class tabbedPage : TabbedPage // muda de contentPage para tabbedPage
+    {
+       
+        public tabbedPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
